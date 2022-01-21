bcb72ef80e321b819c2390cea86f293b285368ef
vinferno <vinsonfernandez@gmail.com>
2022-01-20 19:32:09 -0800

    day04

---
+++ day04/js/main.js
@@ -0,0 +1,26 @@
+let x = 4;
+
+// logical control flow
+// if statement
+if (x == 1) {
+    console.log('this code runs')
+}
+
+// if else statement
+if (x == 1) {
+    console.log('this code runs if true')
+} else {
+    console.log('this code runs if false');
+}
+
+// if else if statement
+
+if (x == 1) {
+    console.log('this code runs if true')
+} else if (x == 2) { 
+    console.log('this code runs if the first condition is false and the second is true');
+} else if (x == 3) {
+    console.log('this code runs if the first and second conditions are false and the third is true');
+} else {
+    console.log('this code runs if all conditions are false');
+}
