121152f0212d70ce85e80409634d523b854f0219
vinferno <vinsonfernandez@gmail.com>
2022-01-20 20:48:44 -0800

    day04

--- day04/js/main.js
+++ day04/js/main.js
@@ -1,4 +1,5 @@
-let x = 4;
+
+let x = 'test';
 
 // logical control flow
 // if statement
@@ -24,3 +25,56 @@ if (x == 1) {
 } else {
     console.log('this code runs if all conditions are false');
 }
+
+// switch statement
+
+switch(x) {
+    case 1:
+        console.log('this code runs if x is 1');
+        break;
+    case 2:
+        console.log('this code runs if x is 2');
+        break;
+    case 3:
+        console.log('this code runs if x is 3');
+        break;
+    case 'test':
+        console.log('this code runs if x is test');
+        break;
+    default:
+        console.log('this code runs if x is not 1, 2, or 3');
+}
+
+// ternary operator
+// shorthand for if else statement
+x == 1 ? console.log('x is 1') : console.log('x is not 1');
+
+
+const bday = prompt('what is your birthday?'); // 1/20
+
+if (Number(bday)) {
+    console.log('great, you entered a number');
+} else {
+    console.log('please enter a number');
+}
+const birthday = new Date(bday); // 1/20/2020
+
+
+const month = birthday.getMonth() === new Date().getMonth(); // true
+const day = birthday.getDate() === new Date().getDate(); // true
+console.log(birthday)
+const correctBday = month && day; // true
+
+if (correctBday) {
+    console.log('happy birthday!');
+} else {
+    console.log('very happy unbirthday!');
+}
+
+// gather information from the user // read
+// store the information in variables // write
+// compare the information/ process the information // update
+// display the result to the user // write
+// delete the information from the variable // delete
+
+// crud: create, read, update, delete
