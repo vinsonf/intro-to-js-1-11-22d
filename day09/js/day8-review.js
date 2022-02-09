2a79bfe89f1239c0c60e9b2b164e6fdc3ed1bff1
vinferno <vinsonfernandez@gmail.com>
2022-02-08 18:53:42 -0800

    changes

--- day09/js/day8-review.js
+++ day09/js/day8-review.js
@@ -5,31 +5,31 @@ class Car {
         this.year = year;
         this.color = color;
     }
-  
+
     display() {
-        const body = document.querySelector('body');
-        const div = document.createElement('div');
+        const body = document.querySelector("body");
+        const div = document.createElement("div");
         div.style.background = this.color;
-        div.style.color = 'white';
-        div.style.display = 'inline-block';
-        div.style.width = '200px';
-        div.style.textAlign = 'center';
-        div.style.margin = '10px'
-        div.style.border = '5px solid black';
+        div.style.color = "white";
+        div.style.display = "inline-block";
+        div.style.width = "200px";
+        div.style.textAlign = "center";
+        div.style.margin = "10px";
+        div.style.border = "5px solid black";
         div.innerHTML = `
-        <hr>
-        Make: ${this.make} <br>
-        Model: ${this.model} <br>
-        Year: ${this.year} <br>
-        Color: ${this.color} <br>
-        <hr>
+            <hr>
+            Make: ${this.make} <br>
+            Model: ${this.model} <br>
+            Year: ${this.year} <br>
+            Color: ${this.color} <br>
+            <hr>
         `;
         body.append(div);
     }
 }
 
-const myCar = new Car('Chevy', 'Corvette', '1955', 'red');
+const myCar = new Car("Chevy", "Corvette", "1955", "red");
 myCar.display();
 
-const myOtherCar = new Car('Ford', 'Mustang', '1964', 'blue');
+const myOtherCar = new Car("Ford", "Mustang", "1964", "blue");
 myOtherCar.display();
